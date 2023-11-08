37a948cb801a695d2869530ea2b06f0988df745e
Karthick <jaikartick@gmail.com>
2023-11-08 20:31:27 +0530

    Initial Commit

---
+++ Amptron/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Amptron;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Amptron/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Amptron;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
